1e519fe9dfb096db12bcd95ce975dadad7b0e07d
Aravindchary18 <aravindchary2007@gmail.com>
2026-02-15 20:37:57 +0530

    Update two_sum.py
    Added Two Sum hashing solution

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -1,1 +1,24 @@
 
+# Two sum - bruteforce approach
+# Time complexity : 0(n^2)
+def two_sum_bruteforce(nums,target):
+    n=len(nums)
+    for i in range(n):
+        for j in range(i+1,n):
+            if nums[i]+nums[j]==target:
+             return[i,j]
+        
+
+# Two sum - optimization approach
+# Time complexity - 0(n)
+def two_sum_optimization(nums,target):
+    seen={}
+    for i in range(len(nums)):
+        needed=target-nums[i]
+        if needed in seen:
+            return [seen[needed],i]
+        seen[nums[i]]=i
+    return[]
+
+print ("brute force:",two_sum_bruteforce([3,5,4,1],7))
+print("optimization:",two_sum_optimization([45,6,6,4],10))
