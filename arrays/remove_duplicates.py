ad8fe1f7af911ca0b9d8ce3376bce7cd26daf753
Aravindchary18 <aravindchary2007@gmail.com>
2026-02-15 21:34:00 +0530

    Update remove_duplicates.py
    added remove duplicates problem

--- arrays/remove_duplicates.py
+++ arrays/remove_duplicates.py
@@ -1,1 +1,13 @@
+def removing_duplicates(nums):
+    seen=set()
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num) 
+    return False
+
+result=removing_duplicates([1,2,3,9,89,4])
+print("result is:",result)
+
+
 
