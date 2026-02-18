ac71e16b82b2cc29507695ed12e178eedd9b4d12
Aravindchary18 <aravindchary2007@gmail.com>
2026-02-18 12:37:12 +0530

    Update max_subarray.py

--- arrays/max_subarray.py
+++ arrays/max_subarray.py
@@ -1,1 +1,11 @@
+def maxsubarray(nums):
+    currentsum=nums[0]
+    maxsum=nums[0]
+    for num in nums[1:]:
+        currentsum=max(num,currentsum+num)
+        maxsum=max(maxsum,currentsum)
+    return maxsum
+result=maxsubarray([1,2,3,4])
+print(result)
+
 
