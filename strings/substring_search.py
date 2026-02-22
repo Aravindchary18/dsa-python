0c4b94a4078d735078018b206b7bc1b3cc12f7c5
Aravindchary18 <aravindchary2007@gmail.com>
2026-02-22 13:53:47 +0530

    Fix substring search function logic and return values
    `Added substring search solution`

--- strings/substring_search.py
+++ strings/substring_search.py
@@ -1,8 +1,8 @@
 def substring_search(string,sub):
      if len(sub)>len(string):
-         return false 
-     for i in range(string-sub+1):
-         if string[i:i+sub]==sub:
-             return true
-     return false
+         return False 
+     for i in range(len(string)-len(sub)+1):
+         if string[i:i+len(sub)]==sub:
+             return True
+     return False
 print(substring_search("abcdef","def"))
